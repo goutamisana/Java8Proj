07792554400e0824f51a5aebf2c08c4097ddc24e
goutami.s <goutami.s@hcl.com>
2017-09-27 19:02:08 +0530

    jj

--- SprinBootSecurity/src/main/java/com/hcl/model/repository/RoleRespository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hcl.model.User.Role;
-
-public interface RoleRespository extends  CrudRepository<Role,Long>{
-
-}
--- SprinBootSecurity/src/main/java/com/hcl/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.model.repository;
-
-import com.hcl.model.User.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long>{
-
-	 User findByUsername(String username);
-}
