fd200712c460e87aa4cdbf3924ff431a07c8fc3e
goutamisana <goutami.s@hcl.com>
2017-09-28 11:57:03 +0530

    removing sysout

--- Java8Proj/src/EnumMain.java
+++ Java8Proj/src/EnumMain.java
@@ -4,7 +4,7 @@ public class EnumMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		system.out.println("inside emummain");
+		
 		System.out.println(EnumExample3.ACTIVE);
 		EnumExample3.ACTIVE.returnStr();
 	}
